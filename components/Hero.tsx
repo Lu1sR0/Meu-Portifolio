0ddd900ae5b886b90666a11dfb9a71e7c00e473d
Lu1sR0 <luiscardoso946@mail.com>
2024-07-01 17:57:48 -0300

    finalizando a seção de meus projetos

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,14 +21,14 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10">
             <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
               <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-                  Bem Vindo ao meu portifolio
+                  Bem Vindo(a) ao meu portifolio👋
               </h2>
               <TextGenerateEffect 
                 className="text-center text-[40px] md:text-5xl lg:text-6xl"
                 words="A cada dia transformando ideias em realidade com um toque de criatividade"
               />
               <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-              Olá, eu sou o Luis Roberto,um desenvolvedor mobile e front-end.
+              Olá, eu sou o Luis Roberto,um desenvolvedor front-end e mobile.
               </p>
                 <a href="#sobre">
                   <MagicButton
